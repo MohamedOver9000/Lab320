a2f1a9d8a1d9282a152506f0d1bdd924b96b2855
LSD04 <sarah.lamini1@gmail.com>
2022-11-30 21:04:40 -0500

    classe employe

--- LAb3/LAb3/Employe.cs
+++ LAb3/LAb3/Employe.cs
@@ -8,5 +8,18 @@ namespace LAb3
 {
     internal class Employe
     {
+        String matricule;
+        String nom;
+        String prenom;
+
+
+        public string Matricule { get => matricule; set => matricule = value; }
+        public string Nom { get => nom; set => nom = value; }
+        public string Prenom { get => prenom; set => prenom = value; }
+
+        public override string ToString()
+        {
+            return nom + " " + prenom;
+        }
     }
 }
