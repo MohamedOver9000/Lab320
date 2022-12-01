9a27b117df481d877bad74ef50d99ccdd17dd27f
Mohamed Amine Ben Daya <mohamed.amine.ben.daya.01@edu.cegeptr.qc.ca>
2022-11-30 20:57:03 -0500

    class projet

--- LAb3/LAb3/Projet.cs
+++ LAb3/LAb3/Projet.cs
@@ -8,5 +8,21 @@ namespace LAb3
 {
     internal class Projet
     {
+        string numero;
+        DateTime debut;
+        int budget;
+        string description;
+        string employe;
+
+        public string Numero { get => numero; set => numero = value; }
+        public DateTime Debut { get => debut; set => debut = value; }
+        public int Budget { get => budget; set => budget = value; }
+        public string Description { get => description; set => description = value; }
+        public string Employe { get => employe; set => employe = value; }
+
+        public override string ToString()
+        {
+            return numero + " " + debut + " " + budget + " " + description + " " + employe;
+        }
     }
 }
